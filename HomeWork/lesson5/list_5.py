f00d120268e9657d529ec9c8be161d85503a8252
ilyasyava <ilyasyava99@gmail.com>
2025-06-20 00:56:22 +0300

    finish_hw_5

--- HomeWork/lesson5/list_5.py
+++ HomeWork/lesson5/list_5.py
@@ -1,7 +1,11 @@
-'''
-Дан список
-['samsung', 'lg', 'xerox', 'bosch']
-Удалить элемент с именем 'xerox'
-Добавить элемент на 2 место 'indesit'
+# '''
+# Дан список
+# ['samsung', 'lg', 'xerox', 'bosch']
+# Удалить элемент с именем 'xerox'
+# Добавить элемент на 2 место 'indesit'
+# '''
+company = ['samsung', 'lg', 'xerox', 'bosch']
+company.remove('xerox')
+company.insert(1, 'indesit')
 
-'''
+print(company)
