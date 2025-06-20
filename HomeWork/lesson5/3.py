4f22059c3c263829d236f2134ec0af2fff695646
ilyasyava <ilyasyava99@gmail.com>
2025-06-20 22:28:19 +0300

    finish_hw_5_1

--- HomeWork/lesson5/3.py
+++ HomeWork/lesson5/3.py
@@ -6,15 +6,6 @@
 d = {'one':11, 'two':22, 'hello':'python', True : False}
 
 el = int(input("Введите номер элемента, который хотите удалить: "))
-if el == 1:
-    el = 'one'
-elif el == 2:
-    el = 'two'
-elif el == 3:
-    el = 'hello'
-elif el == 4:
-    el = True
-
-del d[el]
+del d[list(d.keys())[int(el) - 1]]
 
 print(d)
--- HomeWork/lesson5/list_4.py
+++ HomeWork/lesson5/list_4.py
@@ -4,12 +4,7 @@
 # Вернуть список где вместо элементов данного списка прописаны количество символов каждого элемента.
 # '''
 my_list = ['hello', 'python', 'интерпретатор', 'pep8', "123"]
-my_list_1 = [len(my_list[0]),
-             len(my_list[1]),
-             len(my_list[2]),
-             len(my_list[3]),
-             len(my_list[4])
-]
+my_list_1 = list(map(len, my_list))
 
 print(my_list_1)
 
