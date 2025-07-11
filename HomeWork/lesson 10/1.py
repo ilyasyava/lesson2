abb4d6083bac7e70e5928055f1425a706517557b
ilyasyava <ilyasyava99@gmail.com>
2025-07-11 23:09:41 +0300

    lesson 10, 1-7 ==> finish

--- HomeWork/lesson 10/1.py
+++ HomeWork/lesson 10/1.py
@@ -4,7 +4,63 @@
 #     * или в течении игры по одной через определенное количество кликов
 #     - на каждой забиндить клик через одну и туже функцию cath
 # '''
-from turtle import *
-from random import randint
+import turtle
+import random
+
+from turtle import Turtle, Screen
+from random import choice
+
+COLORS = ['red', 'blue', 'green', 'purple', 'orange', 'pink', 'cyan', 'black']
 
 
+def catch(x, y):
+    print("Черепаха поймана!", x, y)
+
+
+def add_turtle():
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.penup()
+    new_turtle.color(choice(COLORS))
+    new_turtle.goto(0, 0)
+    new_turtle.onclick(catch)
+
+    new_turtle.setheading(random.randint(0, 360))
+    return new_turtle
+
+
+def on_screen_click(x, y):
+    global click_count
+    click_count += 1
+    print("Клики:", click_count)
+
+    if click_count % 4 == 0:
+        new_t = add_turtle()
+        all_turtles.append(new_t)
+
+
+def move_turtles():
+    for t in all_turtles:
+        t.forward(3)
+    screen.ontimer(move_turtles, 50)
+
+
+def main():
+    global click_count, all_turtles, screen
+    click_count = 0
+    all_turtles = []
+
+    screen = turtle.Screen()
+    screen.title("Игра с черепахами")
+    screen.setup(width=600, height=600)
+
+    first_turtle = add_turtle()
+    all_turtles.append(first_turtle)
+
+    screen.onscreenclick(on_screen_click)
+
+    move_turtles()
+
+    screen.mainloop()
+
+
+main()
--- HomeWork/lesson 10/7.py
+++ HomeWork/lesson 10/7.py
@@ -1,8 +1,56 @@
-"""
-**
-задание для самых  любопытных ))
+# """
+# **
+# задание для самых  любопытных ))
+#
+# сделать анимацию снежинок елки с выводом в консоль
+# образец и описание в файле example\elka_animate.py
+# """
+from random import Random
+import time
+import os
 
-сделать анимацию снежинок елки с выводом в консоль
-образец и описание в файле example\elka_animate.py
+random = Random()
 
-"""
+
+def new_sky_line(columns):
+    return ''.join(random.choice('  ..  ''  ""  **') for _ in range(columns))
+
+
+def sky(rows, columns):
+    all_sky = [new_sky_line(columns) for _ in range(rows)]
+    wind = 0
+
+
+    def inner_sky_down_right():
+        nonlocal all_sky, wind
+        all_sky.pop(-1)
+        all_sky.insert(0, new_sky_line(columns))
+        wind = int(wind / 2 + random.randint(-1, 2))
+        all_sky = [line[wind:] + line[:wind] for line in all_sky]
+        return '\n'.join(all_sky)
+    return inner_sky_down_right
+
+
+
+def add_tree(all_sky: list, tree_height):
+    for i in range(tree_height):
+        all_sky[i] = (
+            all_sky[i][: tree_height - i - 1]
+            + "▲" * (i * 2 + 1)
+            + all_sky[i][tree_height + i + 1 :]
+        )
+    return all_sky
+
+
+def print_rastr(rastr_list):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for line in rastr_list:
+        print(line)
+
+sky_gen = sky(30, 80)
+
+while True:
+    raw_sky = sky_gen().split('\n')
+    sky_with_tree = add_tree(raw_sky, 30)
+    print_rastr(sky_with_tree)
+    time.sleep(0.1)
